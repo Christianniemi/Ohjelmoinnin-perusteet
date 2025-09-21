b31f1c3b5df9469d2e3c52a6f66584758e5a5ba2
Christianniemi <christiannniemi@gmail.com>
2025-09-21 17:24:24 +0300

    Delete w1_t7.py

--- w1_t7.py
+++
@@ -1,7 +0,0 @@
-print("Calculate fuel consumption")
-Feed = input("Enter travel distance(kilometers): ")
-Distance = int(Feed)
-input("Enter fuel usage(liters)")
-FuelUsage = int(Feed)
-Consumption = int(200 / 20)
-print("Fuel consumption is", Consumption, "l per 100 km")
